e737402e566ca61f37636e3c0c04b1ac86d0a411
QPC <qperezcastells@gmail.com>
2025-05-13 19:02:56 +0200

    mysc

--- CROMOS/Form2.Designer.vb
+++ CROMOS/Form2.Designer.vb
@@ -23,81 +23,81 @@ Partial Class Form2
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TBoxUsername = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BTrade = New System.Windows.Forms.Button()
+        Me.BVisit = New System.Windows.Forms.Button()
+        Me.LUser = New System.Windows.Forms.Label()
         Me.BLog_out = New System.Windows.Forms.Button()
         Me.BExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(34, 51)
+        Me.Button1.Location = New System.Drawing.Point(28, 79)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(107, 53)
+        Me.Button1.Size = New System.Drawing.Size(182, 81)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'TextBox2
+        'TBoxUsername
         '
-        Me.TextBox2.Location = New System.Drawing.Point(302, 18)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(143, 20)
-        Me.TextBox2.TabIndex = 4
+        Me.TBoxUsername.Location = New System.Drawing.Point(303, 43)
+        Me.TBoxUsername.Name = "TBoxUsername"
+        Me.TBoxUsername.Size = New System.Drawing.Size(143, 20)
+        Me.TBoxUsername.TabIndex = 4
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(31, 18)
+        Me.Label1.Location = New System.Drawing.Point(25, 50)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Label1"
         '
-        'Button2
+        'BTrade
         '
-        Me.Button2.Location = New System.Drawing.Point(211, 51)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(107, 53)
-        Me.Button2.TabIndex = 6
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.BTrade.Location = New System.Drawing.Point(226, 79)
+        Me.BTrade.Name = "BTrade"
+        Me.BTrade.Size = New System.Drawing.Size(107, 81)
+        Me.BTrade.TabIndex = 6
+        Me.BTrade.Text = "Intercanvi"
+        Me.BTrade.UseVisualStyleBackColor = True
         '
-        'Button3
+        'BVisit
         '
-        Me.Button3.Location = New System.Drawing.Point(338, 51)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(107, 53)
-        Me.Button3.TabIndex = 7
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.BVisit.Location = New System.Drawing.Point(339, 79)
+        Me.BVisit.Name = "BVisit"
+        Me.BVisit.Size = New System.Drawing.Size(107, 81)
+        Me.BVisit.TabIndex = 7
+        Me.BVisit.Text = "Veure Cartes"
+        Me.BVisit.UseVisualStyleBackColor = True
         '
-        'Label2
+        'LUser
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(211, 24)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 8
-        Me.Label2.Text = "Label2"
+        Me.LUser.AutoSize = True
+        Me.LUser.Location = New System.Drawing.Point(223, 50)
+        Me.LUser.Name = "LUser"
+        Me.LUser.Size = New System.Drawing.Size(77, 13)
+        Me.LUser.TabIndex = 8
+        Me.LUser.Text = "Buscar usuari: "
         '
         'BLog_out
         '
-        Me.BLog_out.Location = New System.Drawing.Point(34, 137)
+        Me.BLog_out.Location = New System.Drawing.Point(291, 215)
         Me.BLog_out.Name = "BLog_out"
-        Me.BLog_out.Size = New System.Drawing.Size(107, 23)
+        Me.BLog_out.Size = New System.Drawing.Size(88, 23)
         Me.BLog_out.TabIndex = 9
         Me.BLog_out.Text = "Close Session"
         Me.BLog_out.UseVisualStyleBackColor = True
         '
         'BExit
         '
-        Me.BExit.Location = New System.Drawing.Point(147, 138)
+        Me.BExit.Location = New System.Drawing.Point(385, 215)
         Me.BExit.Name = "BExit"
-        Me.BExit.Size = New System.Drawing.Size(107, 22)
+        Me.BExit.Size = New System.Drawing.Size(88, 22)
         Me.BExit.TabIndex = 10
         Me.BExit.Text = "Close"
         Me.BExit.UseVisualStyleBackColor = True
@@ -106,14 +106,14 @@ Partial Class Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(469, 179)
+        Me.ClientSize = New System.Drawing.Size(485, 250)
         Me.Controls.Add(Me.BExit)
         Me.Controls.Add(Me.BLog_out)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.LUser)
+        Me.Controls.Add(Me.BVisit)
+        Me.Controls.Add(Me.BTrade)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TBoxUsername)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form2"
         Me.Text = "Form2"
@@ -123,11 +123,11 @@ Partial Class Form2
     End Sub
 
     Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TBoxUsername As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Label2 As Label
+    Friend WithEvents BTrade As Button
+    Friend WithEvents LUser As Label
+    Friend WithEvents BVisit As Button
     Friend WithEvents BLog_out As Button
     Friend WithEvents BExit As Button
 End Class
--- CROMOS/Form2.vb
+++ CROMOS/Form2.vb
@@ -1,11 +1,16 @@
 ﻿Public Class Form2
+    'declare
+    Dim user As String
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        'initz
+        user = " "
+        TBoxUsername.Text = ""
+        BExit.Visible = True
+        BTrade.Visible = True
+        BVisit.Visible = True
+        BLog_out.Visible = True
     End Sub
 
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
-
-    End Sub
 
     Private Sub BLog_out_Click(sender As Object, e As EventArgs) Handles BLog_out.Click
         Form1.Form1_Load(sender, e)
@@ -19,7 +24,15 @@
         Me.Close()
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub BTrad_Click(sender As Object, e As EventArgs) Handles BTrade.Click
+
+    End Sub
+
+    Private Sub BVisit_Click(sender As Object, e As EventArgs) Handles BVisit.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
     End Sub
 End Class
